46453871dba4f4bda1446e9a00ceb914fad7767f
kokspflanze <kokspflanze@gmx.de>
2014-03-18 22:17:46 +0100

    tickets bearbeiten

--- src/Admin/MainBundle/Controller/TicketSystemController.php
+++ src/Admin/MainBundle/Controller/TicketSystemController.php
@@ -12,7 +12,7 @@ use Symfony\Component\HttpFoundation\Request;
  */
 class TicketSystemController extends Controller {
 	/**
-	 * @Route("/admin/ticketsystem", name="admin_ticketsystem")
+	 * @Route("/admin/ticketsystem/view", name="admin_ticketsystem")
 	 * @Template()
 	 */
 	public function indexAction() {
@@ -24,11 +24,71 @@ class TicketSystemController extends Controller {
 	}
 
 	/**
+	 * @Route("/admin/ticketsystem/view/open", name="admin_ticketsystem_view_open")
+	 * @Template()
+	 */
+	public function viewOpenAction() {
+		$aTicket = $this->getDoctrine()->getRepository('IgelMainBundle:TicketSubject')->findBy(array('type' => 1));
+		return $this->render(
+			'AdminMainBundle:TicketSystem:index.html.twig',
+			array(
+				'aTicket' => $aTicket
+			)
+		);
+	}
+
+	/**
+	 * @Route("/admin/ticketsystem/view/close", name="admin_ticketsystem_view_close")
+	 * @Template()
+	 */
+	public function viewCloseAction() {
+		$aTicket = $this->getDoctrine()->getRepository('IgelMainBundle:TicketSubject')->findBy(array('type' => 2));
+		return $this->render(
+			'AdminMainBundle:TicketSystem:index.html.twig',
+			array(
+				'aTicket' => $aTicket
+			)
+		);
+	}
+
+	/**
 	 * @Route("/admin/ticketsystem/{ticketid}", name="admin_ticketsystem_detail")
 	 * @Template()
 	 */
-	public function detailAction( $ticketid ) {
-		return array(// ...
+	public function detailAction(Request $oRequest) {
+		/**
+		 * @var $oTicketSubject \Igel\MainBundle\Entity\TicketSubject
+		 */
+		$oTicketSubject = $this->getDoctrine()
+			->getRepository('IgelMainBundle:TicketSubject')
+			->findOneBy(array('id' => $oRequest->get('ticketid')));
+
+		if(!(bool) $oTicketSubject){
+			return $this->redirect($this->generateUrl('admin_ticketsystem'));
+		}
+
+		if($oRequest->get('memo') != null){
+			$oManager = $this->getDoctrine()->getManager();
+			// Olli Ticket auf Status Open setzen
+			$oTicketSubject->setType(1);
+			$oManager->persist($oTicketSubject);
+
+			$oTicketEntry = new \Igel\MainBundle\Entity\TicketEntry();
+			$oTicketEntry->setSubject($oTicketSubject);
+			$oTicketEntry->setUser($this->getUser());
+			$oTicketEntry->setMemo($oRequest->get('memo'));
+			$oManager->persist($oTicketEntry);
+
+			$oManager->flush();
+		}
+
+		$aTicketEntry = $this->getDoctrine()
+			->getRepository('IgelMainBundle:TicketEntry')
+			->findBySubject($oTicketSubject, array('created' => 'desc' ));
+
+		return array(
+			'oTicketSubject' => $oTicketSubject,
+			'aTicketEntry' => $aTicketEntry
 		);
 	}
 
